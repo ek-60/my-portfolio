2d592363d2dbe335e573459203c101fec1f788df
ek-60 <101702336+ek-60@users.noreply.github.com>
2023-08-28 15:18:38 +0300

    Update workList.json.ts

--- src/data/workList.json.ts
+++ src/data/workList.json.ts
@@ -6,20 +6,20 @@ export interface Work {
 const hoyry: Work = {
   url: "https://www.svgdoodles.com/",
   time: "May 2023 -",
-  description: " A free collection of 208 super duper SVG Doodles to spice up your online and offline designs!",
+  company: "at Digi- ja mainostoimisto Höyry",
   title: "Technical Services Advisor at Digi- ja mainostoimisto Höyry"
 };
 const migri: Work = {
   url: "https://www.svggradients.com/",
   time: "Jun 2022 - Aug 2022",
-  description: " A free collection of SVG Mesh gradients to use in your design tool or HTML!",
-  title: "Information and Communication Technology Trainee at Finnish Immigration Service"
+  company: "at Finnish Immigration Service",
+  title: "Information and Communication Technology Trainee"
 };
 const fennopoint: Work = {
   url: "https://www.figma.com/community/plugin/1074647052897925336",
   time: "Mar 2019 - Aug 2020",
-  description: " This amazing plugin lets you create empty pages and dividers, no matter what subscription.",
-  title: "B2B sales at Fennopoint Oy"
+  company: "at Fennopoint Oy",
+  title: "B2B sales"
 };
 
 export const byName = {
