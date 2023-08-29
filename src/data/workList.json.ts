d8b2731ca47fedbf95c41f78bce1dd451d2e78d4
ek-60 <101702336+ek-60@users.noreply.github.com>
2023-08-29 15:16:23 +0300

    Update workList.json.ts

--- src/data/workList.json.ts
+++ src/data/workList.json.ts
@@ -6,20 +6,20 @@ export interface Work {
 const hoyry: Work = {
   url: "https://www.svgdoodles.com/",
   time: "May 2023 -",
-  company: "at Digi- ja mainostoimisto Höyry",
-  title: "Technical Services Advisor"
+  company: "Digi- ja mainostoimisto Höyry",
+  title: "Technical Services Advisor at"
 };
 const migri: Work = {
   url: "https://www.svggradients.com/",
   time: "Jun 2022 - Aug 2022",
-  company: "at Finnish Immigration Service",
-  title: "Information and Communication Technology Trainee"
+  company: "Finnish Immigration Service",
+  title: "Information and Communication Technology Trainee at"
 };
 const fennopoint: Work = {
   url: "https://www.figma.com/community/plugin/1074647052897925336",
   time: "Mar 2019 - Aug 2020",
-  company: "at Fennopoint Oy",
-  title: "B2B sales"
+  company: "Fennopoint Oy",
+  title: "B2B sales at"
 };
 
 export const byName = {
